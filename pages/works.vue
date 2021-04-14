9748ccb156281f5354f6f17a9e57caa879d13f96
Joseph Fang <jocstech@gmail.com>
2021-04-13 21:50:09 -0400

    update

--- pages/works.vue
+++ pages/works.vue
@@ -3,37 +3,61 @@
     <PageHeader title="Portfolio and Works" bg="3">
       <p>We provide the professional video production services</p>
     </PageHeader>
-    <section class="container m-auto px-4 py-16">
-      <div class="masonry">
-        <div
-          v-for="(item, index) of portfolios"
-          :key="index"
-          class="item bg-gray-900"
+    <section class="container m-auto px-4 pt-8">
+      <div class="filter flex gap-3">
+        <span
+          class="py-2 px-3 rounded trans hover:bg-gray-200 cursor-pointer"
+          :class="{ 'bg-gray-200': currentCat === null }"
+          @click="changeCat(null)"
         >
-          <nuxt-link :to="`/${item.slug}`">
-            <div class="info py-4 px-3">
-              <h2 class="text-xl font-semibold text-white">{{ item.title }}</h2>
-              <div class="cast">
-                <div class="text-gray-400">
-                  <span>Director:</span>
-                  <span>{{ item.director }}</span>
+          All
+        </span>
+        <span
+          v-for="category in categories"
+          :key="category.id"
+          class="py-2 px-3 rounded trans hover:bg-gray-200 cursor-pointer"
+          :class="{ 'bg-gray-200': currentCat === category.id }"
+          @click="changeCat(category.id)"
+        >
+          {{ category.name }}
+        </span>
+      </div>
+    </section>
+    <section class="container m-auto px-4 pb-16">
+      <div class="masonry">
+        <transition-group name="list" tag="div">
+          <div
+            v-for="item of filteredPortfolios"
+            :key="item.id"
+            class="item trans bg-gray-900"
+          >
+            <nuxt-link :to="`/${item.slug}`">
+              <div class="info py-4 px-3">
+                <h2 class="text-xl font-semibold text-white">
+                  {{ item.title }}
+                </h2>
+                <div class="cast">
+                  <div class="text-gray-400">
+                    <span>Director:</span>
+                    <span>{{ item.director }}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="cover" style="min-height: 200px">
-              <Picture
-                :asset="item.cover"
-                :alt="item.title"
-                :show-play="true"
-              ></Picture>
-            </div>
-            <div class="p-1">
-              <h3 class="text-xs text-gray-200 text-right">
-                Published: {{ new Date(item.created_at).toLocaleString() }}
-              </h3>
-            </div>
-          </nuxt-link>
-        </div>
+              <div class="cover" style="min-height: 200px">
+                <Picture
+                  :asset="item.cover"
+                  :alt="item.title"
+                  :show-play="true"
+                ></Picture>
+              </div>
+              <div class="p-1">
+                <h3 class="text-xs text-gray-200 text-right">
+                  Published: {{ new Date(item.created_at).toLocaleString() }}
+                </h3>
+              </div>
+            </nuxt-link>
+          </div>
+        </transition-group>
       </div>
     </section>
     <section class="bg-gray-100">
@@ -49,17 +73,45 @@
 
 <script>
 export default {
-  components: {},
   async asyncData({ app: { $directus } }) {
     const portfoliosRes = await $directus.items('portfolio').readMany()
+    const categoriesRes = await $directus.items('category').readMany()
     return {
       portfolios: portfoliosRes.data,
+      categories: categoriesRes.data,
     }
+  },
+  data() {
+    return {
+      currentCat: null,
+    }
+  },
+  computed: {
+    filteredPortfolios() {
+      return this.currentCat
+        ? this.portfolios.filter(
+            (portfolio) => portfolio.category === this.currentCat
+          )
+        : this.portfolios
+    },
+  },
+  methods: {
+    changeCat(catId) {
+      this.currentCat = catId
+    },
   },
 }
 </script>
 
 <style lang="scss" scoped>
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.list-complete-leave-active {
+  position: absolute;
+}
 .masonry {
   width: 100%;
   margin: 1.2rem auto;
