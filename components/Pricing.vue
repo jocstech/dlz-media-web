04bf86cec87d5b75125b4850f97f9698005868da
Joseph Fang <jocstech@gmail.com>
2021-04-13 20:18:34 -0400

    update

---
+++ components/Pricing.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="pricing">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+      <div
+        class="tier bg-gradient-to-br from-purple-800 to-blue-500 hover:to-purple-400 text-white px-8 py-16 rounded space-y-6"
+      >
+        <div class="headline text-center space-y-3">
+          <h2 class="text-3xl md:text-5xl">Budget</h2>
+          <h3 class="text-purple-200">Best for start-ups</h3>
+        </div>
+        <hr />
+        <div class="price-number flex items-end justify-center py-2 gap-2">
+          <span class="text-3xl">1200</span>
+          <span class="text-purple-200">CAD/Service</span>
+        </div>
+        <hr />
+        <div class="including space-y-3">
+          <strong>Including Services</strong>
+          <ul class="text-sm list-disc ml-6">
+            <li>HD 1920 X 1080 resolution video</li>
+            <li>Video editing & post-production</li>
+            <li>1 Day video production on location</li>
+          </ul>
+        </div>
+      </div>
+      <div
+        class="tier bg-gradient-to-br from-purple-800 to-blue-500 hover:to-purple-400 text-white px-8 py-16 rounded space-y-6"
+      >
+        <div class="headline text-center space-y-3">
+          <h2 class="text-3xl md:text-5xl">Bronze</h2>
+          <h3 class="text-purple-200">Bestselling</h3>
+        </div>
+        <hr />
+        <div class="price-number flex items-end justify-center py-2 gap-2">
+          <span class="text-3xl">2500</span>
+          <span class="text-purple-200">CAD/Service</span>
+        </div>
+        <hr />
+        <div class="including space-y-3">
+          <strong>Including Services</strong>
+          <ul class="text-sm list-disc ml-6">
+            <li>A 4K resolution video</li>
+            <li>Video editing & post-production</li>
+            <li>Custom creates treatment/ storyboard</li>
+            <li>1 Day video production on locatio</li>
+            <li>Voiceover</li>
+          </ul>
+        </div>
+      </div>
+      <div
+        class="tier bg-gradient-to-br from-purple-800 to-blue-500 hover:to-purple-400 text-white px-8 py-16 rounded space-y-6"
+      >
+        <div class="headline text-center space-y-3">
+          <h2 class="text-3xl md:text-5xl">Silver</h2>
+          <h3 class="text-purple-200">Best for all</h3>
+        </div>
+        <hr />
+        <div class="price-number flex items-end justify-center py-2 gap-2">
+          <span class="text-3xl">3000</span>
+          <span class="text-purple-200">CAD/Service</span>
+        </div>
+        <hr />
+        <div class="including space-y-3">
+          <strong>Including Services</strong>
+          <ul class="text-sm list-disc ml-6">
+            <li>A 4K resolution video</li>
+            <li>Video editing & post-production</li>
+            <li>1 Day video production on location</li>
+            <li>Custom creates treatment/ storyboard</li>
+            <li>Actors/Actress</li>
+            <li>Voiceover</li>
+          </ul>
+        </div>
+      </div>
+      <div
+        class="tier bg-gradient-to-br from-purple-800 to-blue-500 hover:to-purple-400 text-white px-8 py-16 rounded space-y-6"
+      >
+        <div class="headline text-center space-y-3">
+          <h2 class="text-3xl md:text-5xl">Gold</h2>
+          <h3 class="text-purple-200">Best for big company</h3>
+        </div>
+        <hr />
+        <div class="price-number flex items-end justify-center py-2 gap-2">
+          <span class="text-3xl">Contact</span>
+          <span class="text-purple-200">for price</span>
+        </div>
+        <hr />
+        <div class="including space-y-3">
+          <strong>Including Services</strong>
+          <ul class="text-sm list-disc ml-6">
+            <li>Everything in Budget,Bronze,Silver plans</li>
+            <li>Please contact for more information</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/services.vue
+++ pages/services.vue
@@ -4,7 +4,15 @@
       <p>We provide the professional video production services</p>
     </PageHeader>
     <section class="pt-16 space-y-16">
-      <div class="inner container m-auto flex place-items-center px-4">
+      <div class="inner container m-auto flex flex-col px-4 space-y-16">
+        <Headline title="pricing" subtitle="services pricing"></Headline>
+        <div class="content">
+          <Pricing></Pricing>
+        </div>
+      </div>
+    </section>
+    <section class="pt-16 space-y-16">
+      <div class="inner container m-auto px-4">
         <Headline title="services" subtitle="our services"></Headline>
       </div>
       <div class="content">
